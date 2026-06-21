45e9be305c1b41e8ea6acc899ca0c79247ce4192
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-06-21 19:38:57 -0300

    Add new shapes and border size customization

--- main.POO.0.1.py
+++ main.POO.0.1.py
@@ -11,98 +11,155 @@ class Figura:
         pass
 
 class Linha(Figura):
-    def __init__(self, x1, y1, x2, y2, cor_borda):
+    def __init__(self, x1, y1, x2, y2, cor_borda, tamanho_borda):
         super().__init__("", cor_borda)
-        self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.tamanho_borda = tamanho_borda
 
     def desenhar(self, canvas, dash=None):
         canvas.create_line(
             self.x1, self.y1, self.x2, self.y2,
             fill=self.cor_borda,
-            dash=dash
-        )
+            width=self.tamanho_borda,
+            dash=dash)
 
 class Rabisco(Figura):
-    def __init__(self, pontos, cor_borda):
+    def __init__(self, pontos, cor_borda, tamanho_borda):
         super().__init__("", cor_borda)
         self.pontos = pontos
+        self.tamanho_borda = tamanho_borda
 
     def desenhar(self, canvas, dash=None):
         canvas.create_line(
             self.pontos,
             fill=self.cor_borda,
-            dash=dash
-        )
+            width=self.tamanho_borda,
+            dash=dash)
+
+class Retangulo(Figura):
+    def __init__(self, x1, y1, x2, y2, cor_borda, cor_preenchimento, tamanho_borda):
+        super().__init__(cor_preenchimento, cor_borda)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.tamanho_borda = tamanho_borda
+
+    def desenhar(self, canvas, dash=None):
+        canvas.create_rectangle(
+            self.x1, self.y1, self.x2, self.y2,
+            fill=self.cor_preenchimento,
+            outline=self.cor_borda,
+            width=self.tamanho_borda,
+            dash=dash)
+
+class Poligonos(Figura):
+    def __init__(self, x1, y1, x2, y2, x3, y3, cor_borda, cor_preenchimento, tamanho_borda):
+        super().__init__(cor_preenchimento, cor_borda)
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.tamanho_borda = tamanho_borda
+
+    def desenhar(self, canvas, dash=None):
+        canvas.create_polygon(
+            self.x1, self.y1, self.x2, self.y2, self.x3, self.y3,
+            fill=self.cor_preenchimento,
+            outline=self.cor_borda,
+            width=self.tamanho_borda,
+            dash=dash)
 
 class ProgramaPrincipal:
     def __init__(self, janela):
-        #Valores Padrdão
+        # Valores padrão
         self.figuras = []
         self.figura_nova = None
         self.ini_x = 0
         self.ini_y = 0
         self.cor_preenchimento = ""
         self.cor_borda = "black"
-        #Organização 
+        self.tamanho_borda = 1
+        # Organização
         self.janela = janela
         self.janela.title("Brisk - App de Desenhos")
-        
+
         self.icone = PhotoImage(data=ICONE_BASE64)
         self.janela.iconphoto(True, self.icone)
-    
+
         self.organizar = Frame(janela)
         self.organizar.pack()
-        
-        self.tipo_figura_var = StringVar(janela)
 
         self.criar_interface()
         self.criar_canvas()
 
     def criar_interface(self):
-        paddings = {"padx" : 5, "pady" : 5}
-    
+        paddings = {"padx": 5, "pady": 5}
+
         # Rótulo
-        label = ttk.Label(self.organizar,  text='Escolha sua ferramenta')
+        label = ttk.Label(self.organizar, text='Escolha sua ferramenta')
         label.grid(column=0, row=0, sticky=W, **paddings)
 
         # Menu
-        self.tipo_figura_var = StringVar(janela)
+        self.tipo_figura_var = StringVar(self.janela)
         option_menu = ttk.OptionMenu(self.organizar, self.tipo_figura_var,
-                                     'Linha',
-                                     'Linha',
-                                     'Rabisco',
-                                     'Retângulo',
-                                     'Oval',
-                                     'Círculo')
+                                      'Linha',
+                                      'Linha',
+                                      'Rabisco',
+                                      'Retângulo',
+                                      'Oval',
+                                      'Círculo',
+                                      'Poligonos')
 
         preenchimentoCor = ttk.Button(self.organizar, text="Cor de Preenchimento", command=self.escolher_cor_pr)
         bordaCor = ttk.Button(self.organizar, text="Cor da Borda", command=self.escolher_cor_brd)
         limparTela = ttk.Button(self.organizar, text="Limpar Tela", command=self.limpar_tela)
+
+        TamanhoBorda = ttk.Scale(self.organizar, from_=1, to=10, orient=HORIZONTAL)
+        TamanhoBorda.set(1)
+        TamanhoBorda.bind("<ButtonRelease-1>", self.escolher_tamanho_borda)
+
         preenchimentoCor.grid(column=1, row=0, sticky=W, **paddings)
         bordaCor.grid(column=2, row=0, sticky=W, **paddings)
         option_menu.grid(column=3, row=0, sticky=W, **paddings)
         limparTela.grid(column=4, row=0, sticky=W, **paddings)
-    
+        TamanhoBorda.grid(column=5, row=0, sticky=W, **paddings)
+
     def criar_canvas(self):
         # Área de desenho
-        self.canvas = Canvas(self.organizar, bg='white', width=self.janela.winfo_screenwidth(), height=self.janela.winfo_screenheight())
-        self.canvas.grid(column=0, row=1, columnspan=5, sticky=W)
+        self.canvas = Canvas(self.organizar, bg='white',
+                              width=self.janela.winfo_screenwidth(),
+                              height=self.janela.winfo_screenheight())
+        self.canvas.grid(column=0, row=1, columnspan=6, sticky=W)
 
         # Eventos de mouse
         self.canvas.bind('<ButtonPress-1>', self.iniciar_figura_nova)
         self.canvas.bind('<B1-Motion>', self.atualizar_figura_nova)
         self.canvas.bind('<ButtonRelease-1>', self.incluir_figura_nova)
-    
+
     def iniciar_figura_nova(self, event):
         self.ini_x, self.ini_y = event.x, event.y
         tipo = self.tipo_figura_var.get()
 
         if tipo == "Linha":
             self.figura_nova = Linha(event.x, event.y, event.x, event.y,
-                                     self.cor_borda)
+                                      self.cor_borda, self.tamanho_borda)
 
         elif tipo == "Rabisco":
-            self.figura_nova = Rabisco([(event.x, event.y)], self.cor_borda)    
+            self.figura_nova = Rabisco([(event.x, event.y)], self.cor_borda, self.tamanho_borda)
+
+        elif tipo == "Retângulo":
+            self.figura_nova = Retangulo(event.x, event.y, event.x, event.y,
+                                          self.cor_borda, self.cor_preenchimento, self.tamanho_borda)
+
+        elif tipo == "Poligonos":
+            self.figura_nova = Poligonos(event.x, event.y, event.x, event.y, event.x, event.y,
+                                          self.cor_borda, self.cor_preenchimento, self.tamanho_borda)
 
     def atualizar_figura_nova(self, event):
         if not self.figura_nova:
@@ -111,18 +168,20 @@ class ProgramaPrincipal:
         if isinstance(self.figura_nova, Rabisco):
             self.figura_nova.pontos.append((event.x, event.y))
 
+        elif isinstance(self.figura_nova, Poligonos):
+            self.figura_nova.x2 = event.x
+            self.figura_nova.y2 = self.figura_nova.y1
+            self.figura_nova.x3 = (self.figura_nova.x1 + event.x) / 2
+            self.figura_nova.y3 = event.y
+
         else:
             self.figura_nova.x2 = event.x
             self.figura_nova.y2 = event.y
 
         self.desenhar()
-    
+
     def incluir_figura_nova(self, event):
         if self.figura_nova:
-            if isinstance(self.figura_nova, Rabisco):
-                if len(self.figura_nova.pontos) < 2:
-                    self.figura_nova = None
-                    return
             self.figuras.append(self.figura_nova)
             self.figura_nova = None
 
@@ -136,17 +195,19 @@ class ProgramaPrincipal:
 
         if self.figura_nova:
             self.figura_nova.desenhar(self.canvas, dash=(4, 2))
-    
+
     def escolher_cor_pr(self):
         cor = askcolor()[1]
         if cor:
             self.cor_preenchimento = cor
 
-
     def escolher_cor_brd(self):
         cor = askcolor()[1]
         if cor:
             self.cor_borda = cor
+
+    def escolher_tamanho_borda(self, event):
+        self.tamanho_borda = event.widget.get()
 
     def limpar_tela(self):
         self.figuras.clear()
