65823f6d109dac0e7ddb7ea7706ca76eb24f9aad
Leonardo-DePaula <leonardo.depaula@dcomp.ufs.br>
2026-07-06 23:21:41 -0300

    Update figuras.py

--- BRISK/modelo/figuras.py
+++ BRISK/modelo/figuras.py
@@ -17,6 +17,15 @@ class Linha(Figura):
                            fill=self.cor_borda, 
                            width=self.tamEspessura, 
                            dash=dash)
+
+    def paraDicionario(self):
+        return {
+            "tipo": "Linha",
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
+            "cor_borda": self.cor_borda,
+            "tamEspessura": self.tamEspessura
+        }
         
 class Circulo(Figura):
     def __init__(self, x1, y1, x2, y2, cor_borda, cor_preenchimento, tamEspessura):
@@ -32,6 +41,16 @@ class Circulo(Figura):
                            width=self.tamEspessura, 
                            dash=dash)
 
+    def paraDicionario(self):
+        return {
+            "tipo": "Circulo",
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
+            "cor_borda": self.cor_borda,
+            "cor_preenchimento": self.cor_preenchimento,
+            "tamEspessura": self.tamEspessura
+        }
+
 class Oval(Figura):
     def __init__(self, x1, y1, x2, y2, cor_borda, cor_preenchimento, tamEspessura):
         super().__init__(cor_preenchimento, cor_borda)
@@ -44,6 +63,16 @@ class Oval(Figura):
                            outline=self.cor_borda,
                            width=self.tamEspessura,
                            dash=dash)
+
+    def paraDicionario(self):
+        return {
+            "tipo": "Oval",
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
+            "cor_borda": self.cor_borda,
+            "cor_preenchimento": self.cor_preenchimento,
+            "tamEspessura": self.tamEspessura
+        }
         
 class Rabisco(Figura):
     def __init__(self, pontos, cor_borda, tamEspessura):
@@ -54,6 +83,14 @@ class Rabisco(Figura):
     def desenhar(self, canvas, dash=None):
         if len(self.pontos) > 1:
             canvas.create_line(self.pontos, fill=self.cor_borda, width=self.tamEspessura, dash=dash)
+
+    def paraDicionario(self):
+        return {
+            "tipo": "Rabisco",
+            "pontos": self.pontos,
+            "cor_borda": self.cor_borda,
+            "tamEspessura": self.tamEspessura
+        }
 
 class Retangulo(Figura):
     def __init__(self, x1, y1, x2, y2, cor_borda, cor_preenchimento, tamEspessura):
@@ -68,6 +105,16 @@ class Retangulo(Figura):
                                  width=self.tamEspessura, 
                                  dash=dash)
 
+    def paraDicionario(self):
+        return {
+            "tipo": "Retangulo",
+            "x1": self.x1, "y1": self.y1,
+            "x2": self.x2, "y2": self.y2,
+            "cor_borda": self.cor_borda,
+            "cor_preenchimento": self.cor_preenchimento,
+            "tamEspessura": self.tamEspessura
+        }
+
 class Poligono(Figura):
     def __init__(self, pontosPoligonos, cor_borda, cor_preenchimento, tamEspessura):
         super().__init__(cor_preenchimento, cor_borda)
@@ -80,3 +127,54 @@ class Poligono(Figura):
                            outline=self.cor_borda,
                            width=self.tamEspessura, 
                            dash=dash)
+
+    def paraDicionario(self):
+        return {
+            "tipo": "Poligono",
+            "pontosPoligonos": self.pontosPoligonos,
+            "cor_borda": self.cor_borda,
+            "cor_preenchimento": self.cor_preenchimento,
+            "tamEspessura": self.tamEspessura
+        }
+
+
+def criarFiguraDeDicionario(dados):
+ 
+    tipo = dados["tipo"]
+ 
+    match tipo:
+ 
+        case "Linha":
+            return Linha(
+                dados["x1"], dados["y1"], dados["x2"], dados["y2"],
+                dados["cor_borda"], dados["tamEspessura"]
+            )
+ 
+        case "Circulo":
+            return Circulo(
+                dados["x1"], dados["y1"], dados["x2"], dados["y2"],
+                dados["cor_borda"], dados["cor_preenchimento"], dados["tamEspessura"]
+            )
+ 
+        case "Oval":
+            return Oval(
+                dados["x1"], dados["y1"], dados["x2"], dados["y2"],
+                dados["cor_borda"], dados["cor_preenchimento"], dados["tamEspessura"]
+            )
+ 
+        case "Rabisco":
+            return Rabisco(
+                dados["pontos"], dados["cor_borda"], dados["tamEspessura"]
+            )
+ 
+        case "Retangulo":
+            return Retangulo(
+                dados["x1"], dados["y1"], dados["x2"], dados["y2"],
+                dados["cor_borda"], dados["cor_preenchimento"], dados["tamEspessura"]
+            )
+ 
+        case "Poligono":
+            return Poligono(
+                dados["pontosPoligonos"], dados["cor_borda"],
+                dados["cor_preenchimento"], dados["tamEspessura"]
+            )
