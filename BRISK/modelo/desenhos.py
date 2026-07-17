5cbca8ecec3feacf03a3b4d3005054653a2610da
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-07-17 16:13:56 -0300

    Update desenhos.py

--- BRISK/modelo/desenhos.py
+++ BRISK/modelo/desenhos.py
@@ -146,9 +146,5 @@ class Desenhos:
         self.figuras_candidatas = []
         self.retangulo_selecao = None
         self.posicao_anterior = None
-        self.figuras_compostas = [
-            figura for figura in self.figuras
-            if figura.__class__.__name__ == "FiguraComposta"
-        ]
-        
+
         self.desenhar()
