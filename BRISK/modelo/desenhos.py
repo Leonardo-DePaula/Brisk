6277242b8b725abe81b8d3a89871aff0614d04d7
joaopaulo-UFS <paulo.rodrigues@dcomp.ufs.br>
2026-07-17 13:50:23 -0300

    Restore import statements in desenhos.py

--- BRISK/modelo/desenhos.py
+++ BRISK/modelo/desenhos.py
@@ -146,7 +146,6 @@ class Desenhos:
         self.figuras_candidatas = []
         self.retangulo_selecao = None
         self.posicao_anterior = None
-        self.figuras_escolhidas = []
         self.figuras_compostas = [
             figura for figura in self.figuras
             if figura.__class__.__name__ == "FiguraComposta"
